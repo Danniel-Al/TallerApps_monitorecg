3735c895baad02b0622da8c44fd0a1aab8de167b
Daniel Alfaro <A01174359@tec.mx>
2026-04-28 15:47:55 -0600

    feat: Agrega modelo MeasurementRecord para historial en memoria

--- lib/models/measurement_record.dart
+++ lib/models/measurement_record.dart
@@ -1,5 +1,5 @@
 // lib/models/measurement_record.dart
-// MODELO DE DATOS PARA GUARDAR CADA MEDICIÓN REALIZADA
+// MODELO DE DATOS PARA GUARDAR CADA MEDICIÓN
 
 class MeasurementRecord {
   final String id;
@@ -27,36 +27,4 @@ class MeasurementRecord {
     required this.comparisonStatus,
     required this.comparisonText,
   });
-
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'dateTime': dateTime.toIso8601String(),
-      'heartRate': heartRate,
-      'ageRange': ageRange,
-      'gender': gender,
-      'conditions': conditions,
-      'symptoms': symptoms,
-      'medications': medications,
-      'recommendation': recommendation,
-      'comparisonStatus': comparisonStatus,
-      'comparisonText': comparisonText,
-    };
-  }
-
-  factory MeasurementRecord.fromMap(Map<String, dynamic> map) {
-    return MeasurementRecord(
-      id: map['id'],
-      dateTime: DateTime.parse(map['dateTime']),
-      heartRate: map['heartRate'],
-      ageRange: map['ageRange'],
-      gender: map['gender'],
-      conditions: map['conditions'],
-      symptoms: map['symptoms'],
-      medications: map['medications'],
-      recommendation: map['recommendation'],
-      comparisonStatus: map['comparisonStatus'],
-      comparisonText: map['comparisonText'],
-    );
-  }
 }
